f74350f347882542fd9b1fc03ab6a7da6aec5b7e
Michael Okello <okellofrost@gmail.com>
2022-07-14 15:51:07 +0300

    The best fucntion has no parameters

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-let baseSalary = 30000;
+
+//Procedural approach of solving this problem
+let baseSalary = 30_000;
 let overtime = 10 ;
 let rate = 20 ;
+console.log(getWage(baseSalary,overtime,rate));
 
 function getWage(baseSalary , overtime , rate){
 
     return baseSalary + (overtime * rate);
 }
+
+//Object Oriented way
+
+let emmployee = {
+    baseSalary : 30_000,
+    overtime : 10 ,
+    rate : 20 ,
+    getWage:function(){
+        return this.baseSalary + (this.overtime * this.rate)
+    }
+};
+
+const last = emmployee.getWage();
+console.log('OOP',last);
