dda2bec90d585051f014746dffceb4dd145120ef
Michael Okello <okellofrost@gmail.com>
2022-07-14 16:08:06 +0300

    Intro

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
+//Lesson 1 : Encapsulation
+//Reduces complexity by grouping objects together
+//
 //Procedural approach of solving this problem
 let baseSalary = 30_000;
 let overtime = 10 ;
@@ -23,3 +25,13 @@ let emmployee = {
 
 const last = emmployee.getWage();
 console.log('OOP',last);
+
+//Lesson 2 : Abstraction
+//Hide details of complexity & show only essentials
+//reduces complexity and isolates the impact of changes in code
+
+//Lesson 3 : Inheritance
+//Elimiates redundant code
+
+// Lesson 4 : Polymorphism
+//Refactor ugly switch/case statements
